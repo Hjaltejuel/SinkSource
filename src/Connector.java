9237ddbda8202b78428593d162b57e80ee4580d0
Hjaltejuel <hjsk@itu.dk>
2017-10-25 14:42:58 +0200

    almost done

--- src/Connector.java
+++ src/Connector.java
@@ -1,17 +1,155 @@
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.*;
 
 /**
  * Created by Michelle on 25-10-2017.
  */
 public class Connector {
+    static ConcurrentLinkedQueue<Socket> sinks = new ConcurrentLinkedQueue<>();
     public static void main(String[] args) {
-            ConcurrentLinkedQueue<Socket> sinks = new ConcurrentLinkedQueue<>();
+
+
+
+            activateSinks();
+
+            activateSources();
+            Thread SourceSpawner = new Thread(() -> {
+                ServerSocket sourcesInc = null;
+                try {
+                    sourcesInc = new ServerSocket(7000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while (true) {
+                    try {
+                        Socket source = sourcesInc.accept();
+                        System.out.println("A source Process is starting" + '\n');
+                        new Thread(() -> {
+                            Socket currentSocket = null;
+                            BufferedReader inFromSources = null;
+                            try {
+                                inFromSources = new BufferedReader(new InputStreamReader(source.getInputStream()));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            while (true) {
+                                String input = null;
+                                try {
+                                    input = inFromSources.readLine();
+                                } catch (IOException e) {
+                                    System.out.println("Source has been closed" + '\n');
+                                    return;
+                                }
+                                if (input != null) {
+                                    if (!input.equals("null")) {
+                                        System.out.println("A message has been sent from a source, now sending to sinks" + '\n');
+                                        for (Socket socket : sinks) {
+                                            currentSocket = socket;
+                                            DataOutputStream out = null;
+                                            try {
+                                                out = new DataOutputStream(socket.getOutputStream());
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                            try {
+                                                out.writeBytes(input + '\n');
+                                            } catch (SocketException e) {
+                                                sinks.remove(currentSocket);
+                                                System.out.println("A sink has been closed, and will be removed" + '\n');
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+
+
+                        }).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        SourceSpawner.start();
+
+        }
+
+        public static void activateSources(){
+            Thread SourceSpawner = new Thread(() -> {
+                ServerSocket sourcesInc = null;
+                try {
+                    sourcesInc = new ServerSocket(7000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while (true) {
+                    try {
+                        Socket source = sourcesInc.accept();
+                        System.out.println("A source Process is starting" + '\n');
+                        activateSourceThread(source);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        public static void activateSourceThread(Socket source){
+            new Thread(() -> {
+                Socket currentSocket = null;
+                BufferedReader inFromSources = null;
+                try {
+                    inFromSources = new BufferedReader(new InputStreamReader(source.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while (true) {
+                   sendMessages(inFromSources,currentSocket);
+                }
+
+            }).start();
+        }
+        public static void sendMessages(BufferedReader inFromSources,Socket currentSocket){
+            String input = null;
+            try {
+                input = inFromSources.readLine();
+            } catch (IOException e) {
+                System.out.println("Source has been closed" + '\n');
+                return;
+            }
+            if (input != null) {
+                if (!input.equals("null")) {
+                    System.out.println("A message has been sent from a source, now sending to sinks" + '\n');
+                    for (Socket socket : sinks) {
+                        currentSocket = socket;
+                        DataOutputStream out = null;
+                        try {
+                            out = new DataOutputStream(socket.getOutputStream());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.writeBytes(input + '\n');
+                        } catch (SocketException e) {
+                            sinks.remove(currentSocket);
+                            System.out.println("A sink has been closed, and will be removed" + '\n');
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        public static void activateSinks(){
 
             Thread SinkSpawner = new Thread(() -> {
                 ServerSocket sinksInc = null;
@@ -24,55 +162,13 @@ public class Connector {
                     try {
                         Socket sink = sinksInc.accept();
                         sinks.add(sink);
-                        System.out.println("hello" + sinks.size());
+                        System.out.println("A sink process has joined"+'\n');
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             });
-
-
-            Thread SourceSpawner = new Thread(() -> {
-                ServerSocket sourcesInc = null;
-                try {
-                    sourcesInc = new ServerSocket(7000);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                while (true) {
-                    try {
-                        Socket source = sourcesInc.accept();
-
-                        new Thread(() -> {
-                            try {
-                                BufferedReader inFromSources =
-                                        new BufferedReader(new InputStreamReader(source.getInputStream()));
-                                while (true) {
-
-                                    String input = inFromSources.readLine();
-                                    System.out.println("I HAVE RECIEVED :" + input + " AND I AM NOW SENDING TO SINKS");
-                                    int i = 0;
-                                    for (Socket socket : sinks) {
-                                        i++;
-                                        System.out.println(i);
-                                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                                        out.writeBytes(input + '\n');
-                                    }
-                                }
-
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }).start();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        SinkSpawner.start();
-        SourceSpawner.start();
-
+            SinkSpawner.start();
         }
     }
 
