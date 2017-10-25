ecefca758a596043bc132f2ab88a45c97f0ad798
Hjaltejuel <hjsk@itu.dk>
2017-10-25 10:09:23 +0200

    Project made 2

---
+++ src/Sink.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Michelle on 25-10-2017.
+ */
+public class Sink {
+}
--- src/Source.java
+++ src/Source.java
@@ -1,8 +1,23 @@
 /**
  * Created by Michelle on 25-10-2017.
  */
+i
+
 public class Source {
     public static void main(String[] args){
-        System.out.println("hello");
+        try{
+            Context ctx = new InitialContext();
+            TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory)ctx.lookup("TopicConnectionFactory");
+            Topic topic = (Topic)ctx.lookup("Alarms");
+            TopicConnection topicConn =
+            topicConnectionFactory.createTopicConnection();
+            TopicSession topicSess = topicConn.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            TopicSubscriber topicSub = topicSess.createSubscriber(topic);
+            topicSub.start();
+            TextMessage msg = (TextMessage) topicSub.receive();
+            return msg.getText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
